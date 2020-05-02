00cf0f5f89b627fa7fad136cb7b625e0c39e0729
Vinodh Swamy <vinodh.govindaswamy@walmartlabs.com>
2020-05-03 00:24:26 +0530

    Updated the Test App Photos layout to use mix of all  cell size.

--- VSCollectionKit/CollectionKitTestApp/PhotosSectionHandler.swift
+++ VSCollectionKit/CollectionKitTestApp/PhotosSectionHandler.swift
@@ -33,15 +33,121 @@ class PhotosSectionHandler: SectionHandler {
 
     func sectionLayoutProvider(_ sectionModel: SectionModel,
                                _ environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25),
-                                              heightDimension: .fractionalHeight(1))
-        let itemLayout = NSCollectionLayoutItem(layoutSize: itemSize)
-        itemLayout.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
-                                               heightDimension: .fractionalWidth(0.25))
-        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
-                                                             subitems: [itemLayout])
+
+
+        let groupLayout = NSCollectionLayoutGroup.vertical(layoutSize: LayoutSizeInfo.mainGroupLayoutSize,
+                                                           subitems: [fullWidthLayout(),
+                                                                      bigItemWithTwoVerticalPair(),
+                                                                      tripletLayout()])
         let sectionLayout = NSCollectionLayoutSection(group: groupLayout)
         return sectionLayout
     }
+
+    private func fullWidthLayout() -> NSCollectionLayoutItem {
+
+        let itemLayout = NSCollectionLayoutItem(layoutSize: LayoutSizeInfo.FullWidthLayout.fullWidthLayoutSize)
+        itemLayout.contentInsets = LayoutSizeInfo.FullWidthLayout.fullWidthContentInset
+        return itemLayout
+    }
+
+    private func bigItemWithTwoVerticalPair() -> NSCollectionLayoutGroup {
+
+        let mainItemLayout = NSCollectionLayoutItem(layoutSize: LayoutSizeInfo.MainWithTwoPairLayout.mainItemLayoutSize)
+        mainItemLayout.contentInsets = LayoutSizeInfo.MainWithTwoPairLayout.mainItemContentInset
+
+
+        let pairItemLayout = NSCollectionLayoutItem(layoutSize: LayoutSizeInfo.MainWithTwoPairLayout.pairItemSize)
+        pairItemLayout.contentInsets = LayoutSizeInfo.MainWithTwoPairLayout.pairItemContentInset
+
+
+        let pairGroupLayout = NSCollectionLayoutGroup.vertical(layoutSize: LayoutSizeInfo.MainWithTwoPairLayout.pairItemGroupSize,
+                                                               subitem: pairItemLayout,
+                                                               count: 2)
+
+        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: LayoutSizeInfo.MainWithTwoPairLayout.mainGroupSize,
+                                                             subitems: [mainItemLayout, pairGroupLayout])
+        return groupLayout
+    }
+
+    private func tripletLayout() -> NSCollectionLayoutGroup {
+        let itemLayout = NSCollectionLayoutItem(layoutSize: LayoutSizeInfo.TripleItemLayout.tripleItemSize)
+        itemLayout.contentInsets = LayoutSizeInfo.TripleItemLayout.tripleItemContentInset
+
+        let groupLayout = NSCollectionLayoutGroup.horizontal(layoutSize: LayoutSizeInfo.TripleItemLayout.tripleItemGroupSize,
+                                                             subitem: itemLayout,
+                                                             count: 3)
+        return groupLayout
+    }
 }
+
+// ------------------------------------------------------------------------------------ //
+//
+//            FullWidthLayoutItem
+//            ---------------------------------
+//            -                               -
+//            -                               -
+//            -               A               -
+//            -                               -
+//            -                               -
+//            ---------------------------------
+
+//            MainWithTwoPairItem
+//            ---------------------------------
+//            -                  -            -
+//            -                  -     B      -
+//            -                  -            -
+//            -         A        --------------
+//            -                  -            -
+//            -                  -      C     -
+//            -                  -            -
+//            ---------------------------------
+
+//            TripletItem
+//            ---------------------------------
+//            -         -          -          -
+//            -    A    -     B    -     C    -
+//            -         -          -          -
+//            ---------------------------------
+//
+// ------------------------------------------------------------------------------------ //
+
+struct LayoutSizeInfo {
+
+    struct FullWidthLayout {
+        static let fullWidthItemHeight: CGFloat = 200
+        static let fullWidthLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                                heightDimension: .absolute(fullWidthItemHeight))
+        static let fullWidthContentInset = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 0, trailing: 2)
+    }
+
+    struct MainWithTwoPairLayout {
+        static let mainItemHeight: CGFloat = 172
+        static let mainItemLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(2/3),
+                                                               heightDimension: .absolute(mainItemHeight))
+        static let mainItemContentInset = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 0, trailing: 0)
+
+        static let pairItemHeight: CGFloat = mainItemHeight / 2
+        static let pairItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                  heightDimension: .fractionalHeight(1))
+        static let pairItemContentInset = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 0, trailing: 0)
+        static let pairItemGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/3),
+                                                              heightDimension: .absolute(pairItemHeight * 2))
+        static let mainGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                          heightDimension: .absolute(mainItemHeight))
+    }
+
+    struct TripleItemLayout {
+        static let tripleItemHeight: CGFloat = 86
+        static let tripleItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                           heightDimension: .fractionalHeight(1))
+        static let tripleItemContentInset = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 0, trailing: 0)
+        static let tripleItemGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                                heightDimension: .absolute(tripleItemHeight))
+    }
+
+    static let mainGroupLayoutSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                                            heightDimension: .absolute(FullWidthLayout.fullWidthItemHeight +
+                                                                MainWithTwoPairLayout.mainItemHeight +
+                                                                TripleItemLayout.tripleItemHeight))
+}
+
